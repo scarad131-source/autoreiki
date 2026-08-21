c4302303a3c58745b8cda9756570591fc0c39ee2
Silja Carad <scarad131@gmail.com>
2026-08-21 21:51:51 +0000

    Actualizar icono de Meditar a flor de loto de siete pétalos

--- src/components/LotusIcon.jsx
+++ src/components/LotusIcon.jsx
@@ -11,19 +11,19 @@ export default function LotusIcon({ className = "", strokeWidth = 2, ...props })
       className={className}
       {...props}
     >
-      {/* borde circular */}
-      <circle cx="12" cy="12" r="8.5" />
-      {/* pétalo central (más alto) */}
-      <path d="M12 12 C 11 9.5 11 7 12 5 C 13 7 13 9.5 12 12" />
+      {/* pétalo central */}
+      <path d="M12 14 C 10.8 10 10.8 6.5 12 3 C 13.2 6.5 13.2 10 12 14" />
       {/* pétalos internos */}
-      <path d="M12 12 C 10.5 10.5 9.2 8.8 8.5 7 C 9.8 8.2 11 9.8 12 12" />
-      <path d="M12 12 C 13.5 10.5 14.8 8.8 15.5 7 C 14.2 8.2 13 9.8 12 12" />
-      {/* pétalos exteriores (más bajos) */}
-      <path d="M12 12 C 9.5 11.2 7.2 10.8 5.5 9.5 C 7.5 9.8 9.8 10.2 12 12" />
-      <path d="M12 12 C 14.5 11.2 16.8 10.8 18.5 9.5 C 16.5 9.8 14.2 10.2 12 12" />
-      {/* rayos de la base */}
-      <path d="M9.4 20.2 7.6 22" />
-      <path d="M14.6 20.2 16.4 22" />
+      <path d="M12 14 C 10.5 11.5 9.2 8.5 8.5 5.5 C 10 8 11.2 11 12 14" />
+      <path d="M12 14 C 13.5 11.5 14.8 8.5 15.5 5.5 C 14 8 12.8 11 12 14" />
+      {/* pétalos medios */}
+      <path d="M12 14 C 9.5 12.8 7 11 5.5 8 C 7.5 9.8 10 11.5 12 14" />
+      <path d="M12 14 C 14.5 12.8 17 11 18.5 8 C 16.5 9.8 14 11.5 12 14" />
+      {/* pétalos exteriores */}
+      <path d="M12 14 C 8.5 13.5 6 12.5 3.5 11 C 6 12 9 13 12 14" />
+      <path d="M12 14 C 15.5 13.5 18 12.5 20.5 11 C 18 12 15 13 12 14" />
+      {/* base curva que acuna la flor */}
+      <path d="M3 15.5 Q 12 20.5 21 15.5" />
     </svg>
   );
 }
